b3e8bb701718a08b0fc370df45eb80c202d6277f
subhadeep <androdeep25@gmail.com>
2023-10-29 16:52:08 +0530

    theme modified

--- app/src/main/java/com/example/myportfolioadminapp/MainActivity.kt
+++ app/src/main/java/com/example/myportfolioadminapp/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myportfolioadminapp.ui.theme.MyPortfolioAdminAppTheme
+import com.example.myportfolioadminapp.common.ui.theme.MyPortfolioAdminAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
